1b4c06013f00877327a6de3c4045c9310e9f2198
Kielcy <charleschristopher.organista@lorma.edu>
2025-04-03 00:49:20 +0800

    makapuyat talaga

---
+++ src/test/java/com/example/MIDTERMS_exam/MidtermsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MIDTERMS_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
